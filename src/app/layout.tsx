8aa1ad3923af024e6dcc9bd8e4eeabba208a0486
Alok Mishra <hey.alokmishra@gmail.com>
2026-07-24 19:19:21 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next"
+
+export const dynamic = "force-dynamic"
 import { Suspense } from "react"
 import { Geist_Mono, Inter, Pixelify_Sans } from "next/font/google"
 
